ad44aac6dc3168698792ccdcab27c3f286a4d2d5
unknown <keegood8@gmail.com>
2019-10-04 19:18:28 -0700

    corrected variable name error in version3

--- madlibs/version3.py
+++ madlibs/version3.py
@@ -18,7 +18,7 @@ while(game_over == False):
     verb_1 = gerunds[0]
     verb_2 = gerunds[1]
 
-    f"\nResult: \n\nIf you go to some {adjective_1} place like {place} , you must know how to deal with wild animals such as bears, wolves and{plural_noun} . The most important of these is the bear. There are three kinds of bear, the grizzly bear, the {adjective_2} bear and the {adjective_3} bear. Bears spend most of their time {verb_1} or {verb_2} . They look very {adjective_4} , but if you make them {adjective_5} , they might bite your {noun_1} ."
+    f"\nResult: \n\nIf you go to some {adjective_1} place like {place} , you must know how to deal with wild animals such as bears, wolves and{plural_noun} . The most important of these is the bear. There are three kinds of bear, the grizzly bear, the {adjective_2} bear and the {adjective_3} bear. Bears spend most of their time {verb_1} or {verb_2} . They look very {adjective_4} , but if you make them {adjective_5} , they might bite your {noun} ."
 
     play_again = input("\n\nWould you like to play again? Enter \"y\" for Yes and \"n\" for No: ")
 
