4d0d69b75a771cc1c3897ce38e6cfd8e8cce7f77
unknown <keegood8@gmail.com>
2019-10-04 21:02:03 -0700

    version 2. Added ability for user to select value for number of characters generated

--- random_password/random_password.py
+++ random_password/random_password.py
@@ -1,11 +1,19 @@
 import string
 import random
 
-n = 16
-x = 0
-password = ""
-while x < n:
-    char = random.choice(string.ascii_letters)
-    password += char
-    x += 1
-print(password)
+def display_password(pwd):
+    print(pwd)
+
+def main():
+    print("\nWelcome to the random password generator.")
+    n = int(input("\nPlease enter the number of characters you'd like your password to be: "))
+
+    x = 0
+    password = ""
+    while x < n:
+        char = random.choice(string.ascii_letters)
+        password += char
+        x += 1
+    display_password(password)
+
+main()
