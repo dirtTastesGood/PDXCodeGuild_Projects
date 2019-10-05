da8d187591ce4d46e9be80b17d45c7659e3c4722
unknown <keegood8@gmail.com>
2019-10-04 20:18:20 -0700

    version2

--- magic8Ball/magic8ball.py
+++ magic8Ball/magic8ball.py
@@ -1,10 +1,20 @@
 import random
-options = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes","Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
 
+def answerQuestion():
+    options = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes","Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
 
-print("\nCome one, come all, look through truth's eye! Ask the Magic 8-ball your questions!")
-question = input("Type your question here or enter \'done\' to exit: ")
+    choice = random.choice(options)
 
-choice = random.choice(options)
+    return choice
+def main():
+    while True:    
+    
+        print("\nCome one, come all, look through truth's eye! Ask the Magic 8-ball your questions!")
 
-print(choice)
+        question = input("Type your question here or enter \'done\' to exit: ")
+
+        if question.lower() == "done":
+            break
+        else: 
+            print(answerQuestion())
+main()
