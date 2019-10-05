2fda487b11a909d80405713cac87d0210dc012d4
unknown <keegood8@gmail.com>
2019-10-04 19:42:52 -0700

    version 2. added while loop for repeated gameplay and broke into functions

--- magic8Ball/magic8Ball.py
+++ magic8Ball/magic8Ball.py
@@ -1,14 +1,22 @@
 import random
 
-options = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes","Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
+def answerQuestion():
 
-print("\nCome one, come all, look through truth's eye! Ask the Magic 8-ball your questions!")
-while True:    
-    input("Type your question here: ")
+    options = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes","Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
 
     choice = random.choice(options)
 
-    print(choice)
+    return choice
 
-    if input("Ask another question? y/n: ").lower() == "n":
-        break
+def main():
+    print("\nCome one, come all, look through truth's eye! Ask the Magic 8-ball your questions!")
+
+    while True:    
+        question = input("\nType your question here or enter \'done\' to exit: ")
+
+        if question.lower() == "done":
+            break
+        else:
+            print(answerQuestion())
+
+main()
