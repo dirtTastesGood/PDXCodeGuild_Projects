a7733f8d7709e959b5087a014b334192ee609b89
unknown <keegood8@gmail.com>
2019-10-04 19:31:10 -0700

    magic 8 ball version1 complete

---
+++ magic8Ball/magic8Ball.py
@@ -0,0 +1,14 @@
+import random
+
+options = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes","Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
+
+print("\nCome one, come all, look through truth's eye! Ask the Magic 8-ball your questions!")
+while True:    
+    input("Type your question here: ")
+
+    choice = random.choice(options)
+
+    print(choice)
+
+    if input("Ask another question? y/n: ").lower() == "n":
+        break
