5bb3220ec257d3b0e7c94130daa88147a5d9ced0
unknown <keegood8@gmail.com>
2019-10-05 20:57:35 -0700

    version3. user selects number of upper and lowercase letters

--- random_password/random_password.py
+++ random_password/random_password.py
@@ -2,18 +2,33 @@ import string
 import random
 
 def display_password(pwd):
-    print(pwd)
+    print(f"Your new password is {pwd}")
+
+def getChars(letter_set, num):
+    x = 0
+    chars = ""
+    while x < num:
+        char = random.choice(letter_set)
+        chars += char
+        x += 1
+    return chars
+
+def buildPassword(uppers, lowers):
+    charSet = list(uppers + lowers)
+    pwd = "".join(charSet)
+    
+    return pwd
 
 def main():
     print("\nWelcome to the random password generator.")
-    n = int(input("\nPlease enter the number of characters you'd like your password to be: "))
+    uppercaseNum = int(input("\nPlease selecte the number of uppercase characters you'd like your password to contain: "))
+    lowercaseNum = int(input("\nPlease selecte the number of lowercase characters you'd like your password to contain: "))
+   
+    bigs = getChars(string.ascii_uppercase, uppercaseNum)
+    littles = getChars(string.ascii_lowercase, lowercaseNum)
 
-    x = 0
-    password = ""
-    while x < n:
-        char = random.choice(string.ascii_letters)
-        password += char
-        x += 1
+    password = buildPassword(bigs, littles)
+    
     display_password(password)
 
 main()
