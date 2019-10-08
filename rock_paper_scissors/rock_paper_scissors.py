36030b9fdcf04a8f52235724bb1ff75c67f57a51
unknown <keegood8@gmail.com>
2019-10-07 18:46:47 -0700

    version2 add while loop for continued play. split into functions

--- rock_paper_scissors/rock_paper_scissors.py
+++ rock_paper_scissors/rock_paper_scissors.py
@@ -1,17 +1,40 @@
 import random
 throws = ["rock", "paper", "scissors"]
 
-print("\n Let's play Rock, Paper, Scissors!")
-user_throw = input("\n Please choose rock, paper, or scissors: ").lower()
 
-comp_throw = random.choice(throws)
 
-if user_throw == comp_throw:
-    outcome = "You tied with the computer!"
-elif (user_throw == "scissors" and comp_throw == "paper") or (user_throw == "paper" and comp_throw == "rock") or (user_throw == "rock" and comp_throw == "scissors"):
-    outcome = "You won!"
-elif (user_throw == "paper" and comp_throw == "scissors") or (user_throw == "rock" and comp_throw == "paper") or (user_throw == "rock" and comp_throw == "paper"):
-    outcome = "The computer won!"
 
-print(f"The computer threw {comp_throw}. You threw {user_throw}.")
-print(outcome)
+def main():
+    
+
+    while True:
+        print("\n Let's play Rock, Paper, Scissors!")
+        user_throw = input("\n Please choose rock, paper, or scissors ('q' to quit): ").lower()
+
+        comp_throw = comp_choose()
+        outcome = get_outcome(comp_throw, user_throw)
+        print_outcome(outcome, comp_throw, user_throw)
+        if user_throw == 'q':
+            break
+def comp_choose():
+    throws = ["rock", "paper", "scissors"]
+
+    comp_throw = random.choice(throws)
+
+    return comp_throw
+
+def get_outcome(comp_throw, user_throw):
+    if user_throw == comp_throw:
+        outcome = "You tied with the computer!"
+    elif (user_throw == "scissors" and comp_throw == "paper") or (user_throw == "paper" and comp_throw == "rock") or (user_throw == "rock" and comp_throw == "scissors"):
+        outcome = "You won!"
+    elif (user_throw == "paper" and comp_throw == "scissors") or (user_throw == "rock" and comp_throw == "paper") or (user_throw == "rock" and comp_throw == "paper"):
+        outcome = "The computer won!"
+
+    return outcome
+
+def print_outcome(outcome, comp_throw, user_throw):
+    print(f"The computer threw {comp_throw}. You threw {user_throw}.")
+    print(outcome)
+
+main()
