9dab1579921a84072c70221fafb553dad6e2ef0b
unknown <keegood8@gmail.com>
2019-10-08 19:29:33 -0700

    version 2. User enters units to be converted to meters

--- unit_converter/unit_converter.py
+++ unit_converter/unit_converter.py
@@ -1,15 +1,46 @@
 def convert_units(value, unit_in, unit_out):
-    conversion_sets = {"feet":{"meters": 0.3048}}
-
-    conversion_rate = conversion_sets[unit_in][unit_out]
+    conversions = {
+                    "feet":
+                            {
+                             "feet": 1,
+                             "kilometers": 0.0003048,
+                             "meters": 0.3048,
+                             "miles":  0.00018939393939394
+                            },
+                    "kilometers": 
+                            {
+                             "feet": 3280.8399,
+                             "kilometers": 1,
+                             "meters": 1000,
+                             "miles": 0.62137
+                            },
+                    "meters":
+                            {
+                             "feet": 3.2808399,
+                             "kilometers": 0.001,
+                             "meters": 1,
+                             "miles": 0.000621371192                              
+                            },
+                    "miles":
+                            {
+                             "feet": 5280, 
+                             "kilometers": 1.609344,
+                             "meters": 1609.344,
+                             "miles": 1
+                            }
+                      }
+    conversion_rate = conversions[unit_in][unit_out]
     new_value = value * conversion_rate
 
     return new_value
 
 def main():
+
+    print("\nLet's convert some units!")
+
     while True:
-        unit1 = "feet"
-        unit2 = "meters"
+        unit1 = input("\nEnter the starting unit: ")
+        unit2 = "meters" #input(f"\nEnter the unit into which you'd like to convert {unit1}: ")
 
         num_of_unit1 = float(input(f"\nPlease enter the number of {unit1} you'd like converted to {unit2} ('q' to quit): "))
         num_of_unit2 = convert_units(num_of_unit1, unit1, unit2)
