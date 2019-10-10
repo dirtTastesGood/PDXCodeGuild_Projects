39c3142eca29f0c7dbcff7cdfaf0b3020cf93bac
unknown <keegood8@gmail.com>
2019-10-09 20:46:19 -0700

    version 3. Add distance comparison of previous and current guesses to target

--- guess_the_number/guess_the_number.py
+++ guess_the_number/guess_the_number.py
@@ -6,22 +6,51 @@ def check_guess(guess, answer):
     else: 
         return False
 
+def high_low(guess, answer):
+    if guess < answer:
+        msg = "Your guess is too low!"
+    elif guess > answer:
+        msg = "Your guess is too high!"
+    
+    return msg
+
+def error_margin(curr_guess, prev_guess, answer):
+    prev_margin = abs(prev_guess - answer)
+    curr_margin = abs(curr_guess - answer)
+
+    if curr_margin > prev_margin:
+        msg = f"Your guess of {curr_guess} is further from the answer than your last guess of {prev_guess}."
+    elif curr_margin < prev_margin:
+        msg = f"Your guess of {curr_guess} is closer to the answer than your last guess of {prev_guess}."
+    else:
+        msg = ""
+    return msg
+
 def main():
     answer = random.randint(1,10)
     
     print("Welcome! The computer has chosen a number between 1 and 10. Try to guess the number in ten tries or less!")
 
     guesses = []
+    prev_guess = answer
 
     while True: 
         guess = int(input(f"\nEnter guess ('q' to quit): "))
         guesses.append(guess)
-        
+
         if check_guess(guess, answer) == True:
             print(f"\nYou guessed correctly! {guess} is the number the computer chose. \nGreat job! \nNumber of guesses: {len(guesses)}")
             break
         elif check_guess(guess, answer) == False:
-            print(f"{guess} is not the correct number. Try again.")
+
+            hi_lo = high_low(guess,answer)
+            accuracy = error_margin(guess, prev_guess, answer)
+            prev_guess = guess
+
+            print(f"{guess} is not the correct number. {hi_lo}.")
+            if len(guesses) > 1:
+                print(f"{accuracy} Try again.")
+
         
         if str(guess).lower() == "q":
             break
