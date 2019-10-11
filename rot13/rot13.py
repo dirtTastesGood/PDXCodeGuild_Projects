d62a63a4353364714410e6dad4b5654dc27562d9
unknown <keegood8@gmail.com>
2019-10-10 19:38:18 -0700

    version 2. User defines the string's offset

--- rot13/rot13.py
+++ rot13/rot13.py
@@ -1,27 +1,33 @@
 import string
 
-def get_rot13(user_string):
-
+def rotate_string(user_string, direction, offset):
     user_string = user_string.replace(" ", "")
     english = list(string.ascii_lowercase)
     
-    rot13 = english[13::] # letters 14-26
-    p2 = english[0:13] # letters 1 - 13
-    rot13.extend(p2) # move beginning of list to end of list
+    if direction == "d":
+        offset = 26 - offset
+
+    rotated = english[offset::] # letters (offset+1)-26
+    p2 = english[0:offset] # letters 1 - offset
+    rotated.extend(p2) # move beginning of list to end of list
 
     holder = []
     for char in list(user_string):
         index = english.index(char)
-        holder.append(rot13[index])
+        holder.append(rotated[index])
         cipher = ''.join(holder)
 
     return cipher
 
 def main():
-    print("Welcome to the ROT 13 cipher encrypter/decrypter!")
+    print("Welcome to the rotation cipher encrypter/decrypter!")
 
+    direction = input("\nWhat would you like to do? \nEnter 'e' for encrypt or 'd' for decrypt: ")
     string = input("\nPlease enter a string with lowercase letters only: ")
+    offset = int(input("\nPlease enter the number of characters your string should be shifted: "))
+    
+    cipher = rotate_string(string, direction, offset)
 
-    cipher = get_rot13(string)
     print(f"Your string \"{string}\" converts to \"{cipher}\".")
+
 main()
