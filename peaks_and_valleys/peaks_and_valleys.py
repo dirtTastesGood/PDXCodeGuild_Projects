0bbab99c5f073011f3612164a0bed4ba822ae559
unknown <keegood8@gmail.com>
2019-10-11 21:09:04 -0700

    Find peaks and valleys within a list of integers

--- peaks_and_valleys/peaks_and_valleys.py
+++ peaks_and_valleys/peaks_and_valleys.py
@@ -23,12 +23,39 @@ def find_peaks_and_valleys(peaks, valleys):
 
     return peaks_and_valleys
 
+def largest_peak(data):
+    largest = 0
+    for datum in data:
+        if datum > largest:
+            largest = datum
+    
+    return largest
+
+def display(data):
+    graph = []
+    top = largest_peak(data)
+
+    for i, datum in enumerate(data):
+        row = []
+        while len(row) < top:
+            if len(row) < datum:
+                row.append("X")
+            else:
+                row.append(" ")
+            
+        graph.append(row)
+    
+    for row in graph:
+        print(row)
+
 def main():
     data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
     
     peaks = find_peaks(data)
     valleys = find_valleys(data)
     peaks_and_valleys = find_peaks_and_valleys(peaks, valleys)
+
+    display(data)
 
     print(f"Peaks: {peaks}")
     print(f"Valleys: {valleys}")
