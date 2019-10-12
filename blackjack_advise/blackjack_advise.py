a4dd0e03cd9e0e43a65d3fd31fb29200e97b54df
unknown <keegood8@gmail.com>
2019-10-12 15:40:15 -0700

    version 2. change point value of aces to 1 or 11  based on total score

--- blackjack_advise/blackjack_advise.py
+++ blackjack_advise/blackjack_advise.py
@@ -15,18 +15,20 @@ def get_score(cards):
                    "A": 11
                   }
     score = 0
-
     for card in cards:
         card.lower()
+        if card == 'a' and (score + card_scores['A']) <= 21:
+            card = 'A'
+        
         score += card_scores[card]
 
     return score
 
 def score_advise(score):
     if score < 17:
-        advise = "You should hit."
-    elif 17 < score < 21:
-        advise = "You should stay."
+        advise = "You should proabably hit."
+    elif 17 <= score < 21:
+        advise = "You should probably stay."
     elif score == 21:
         advise = "You've already acheived Blackjack"
     elif score > 21:
@@ -39,10 +41,9 @@ def main():
 
     print("\nWelcome to Blackjack Adviser. Please enter three cards (A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, or K)")
     
-    cards.append(input("\nPlease enter your first card: "))
-    cards.append(input("\nPlease enter your second card: "))
-    cards.append(input("\nPlease enter your third card: "))
-    
+    cards.append(input("\nPlease enter your first card: ").lower())
+    cards.append(input("\nPlease enter your second card: ").lower())
+    cards.append(input("\nPlease enter your third card: ").lower())
     score = get_score(cards)
 
     advise = score_advise(score)
