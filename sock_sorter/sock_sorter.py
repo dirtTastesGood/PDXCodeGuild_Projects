00213006304010cd329e48df9daead209827d4a9
unknown <keegood8@gmail.com>
2019-10-13 15:09:56 -0700

    version 2. randomly assign one of three colors to each sock. Then sort by pairs and singles of each color of each type.

--- sock_sorter/sock_sorter.py
+++ sock_sorter/sock_sorter.py
@@ -2,16 +2,19 @@ from random import randint
 
 def buy_socks():
     sock_types = ['ankle', 'crew', 'calf', 'thigh']
+    sock_colors = ['black', 'white', 'blue']
     socks = {}   
 
     for sock in sock_types:
         socks[sock] = {}
-        socks[sock] = {'quantity': 0, 'pairs':0, 'singles':0}
+        for color in sock_colors:
+            socks[sock][color] = {'quantity': 0, 'pairs':0, 'singles':0}
 
     i = 0
     while i < 100:
         sock_type = sock_types[randint(0, len(sock_types)-1)]
-        socks[sock_type]['quantity'] += 1
+        sock_color = sock_colors[randint(0, len(sock_colors)-1)]
+        socks[sock_type][sock_color]['quantity'] += 1
         i += 1
     
     return socks
@@ -20,8 +23,8 @@ def pair_socks(socks):
     for sock in socks:
         for color in socks[sock]:
             
-            socks[sock]['pairs'] = socks[sock]['quantity'] // 2
-            socks[sock]['singles'] = socks[sock]['quantity'] % 2
+            socks[sock][color]['pairs'] = socks[sock][color]['quantity'] // 2
+            socks[sock][color]['singles'] = socks[sock][color]['quantity'] % 2
 
 
     return socks        
@@ -34,7 +37,8 @@ def main():
     pair_socks(socks)
     for sock in socks:
         print(f"{sock}: ")
-        print(f"    quantity: {socks[sock]['quantity']}")
-        print(f"        pairs: {socks[sock]['pairs']}\n        singles: {socks[sock]['singles']}")
-        
+        for color in socks[sock]:
+            print(f"    {color}:\n        quantity: {socks[sock][color]['quantity']}")
+            print(f"            pairs: {socks[sock][color]['pairs']}\n            singles: {socks[sock][color]['singles']}")
+        pass    
 main()
