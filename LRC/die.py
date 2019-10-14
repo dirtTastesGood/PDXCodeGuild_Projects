c9442cf1c931499b070f39d3bb1a970e13fa2413
unknown <keegood8@gmail.com>
2019-10-13 21:19:51 -0700

    define game, player and die classes

---
+++ LRC/die.py
@@ -0,0 +1,6 @@
+class Die:
+    def __init__(self):
+        self.faces = ["dot", "dot", "dot", "L", "C", "R"]
+    
+
+
---
+++ LRC/game.py
@@ -0,0 +1,8 @@
+from die import Die
+from player import Player
+
+def main():
+    d = Die()
+    p = Player("keegan")
+
+main()
