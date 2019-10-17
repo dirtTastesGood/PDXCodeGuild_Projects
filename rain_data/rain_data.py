e60ce630ec8cda7b2dc457f74e431949bdd0430a
unknown <keegood8@gmail.com>
2019-10-16 23:12:58 -0700

    calculate total average over all dates

--- rain_data/rain_data.py
+++ rain_data/rain_data.py
@@ -1,13 +1,13 @@
 def load_data(data):
     with open(data, 'r') as rain_data:
         data = rain_data.readlines()
-        data_tups = []
+        lines = []
         for line in data:
             line = line.split(" ")
             line = [x for x in line if x != ""] # eliminate blank strings in line
-            data_tups.append((line[0], line[1]))
+            lines.append([line[0], line[1]])
 
-    return data_tups
+    return lines
 
 def create_date_dict(data):
     date_dict = {}
@@ -32,6 +32,15 @@ def create_date_dict(data):
             
     return date_dict
 
+def average(data):
+    nums = []
+    for i in data:
+        if i[1] != '-':
+            nums.append(int(i[1]))
+    avg = sum(nums) // len(nums)
+
+    return avg
+    
 def main():
     raw_data = "rain_data.txt"
     data = load_data(raw_data)
@@ -53,5 +62,7 @@ def main():
 
     print(day)
     print(f"The total rainfall for {month}-{day}-{year} was {rain_data[year][month][day]}")
+
+    print(f"The average total rainfall over the entire data set is: {average(data)}")
 main()
 
