5459679376b2e9cece2716e68176129d1cf15351
unknown <keegood8@gmail.com>
2019-10-16 21:21:25 -0700

    so close. losing days 30 and 31 in create_date_dict()

--- rain_data/rain_data.py
+++ rain_data/rain_data.py
@@ -1,5 +1,3 @@
-import datetime
-
 def load_data(data):
     with open(data, 'r') as rain_data:
         data = rain_data.readlines()
@@ -8,28 +6,58 @@ def load_data(data):
             line = line.split(" ")
             line = [x for x in line if x != ""] # eliminate blank strings in line
             data_tups.append((line[0], line[1]))
+
     return data_tups
 
 def create_date_dict(data):
     date_dict = {}
 
     for datum in data:
-        if len(datum) > 3:
-            year = datum[-4::]
-            month = datum[-8:-5]
-            day = datum[0:2]
+        split_date = datum[0].split("-")
+        
+        year = split_date[2]
+        month = split_date[1]
+        day = split_date[0]
 
-    print(data)
+        rainfall = datum[1]
+
+        if year not in date_dict:
+            date_dict[year] = {}
+        elif year in date_dict:
+            if month not in date_dict[year]:
+                date_dict[year][month] = {} 
+            elif month in date_dict[year]:
+                if day not in date_dict[year][month]:
+                    date_dict[year][month][day] = rainfall
+            
+    
+
+    return date_dict
 
 def main():
     raw_data = "rain_data.txt"
     data = load_data(raw_data)
+    rain_data = create_date_dict(data)
 
-    user_date = "08-OCT-2019"
+    #print(rain_data)
+    #print(rain_data['2011']['AUG']['30'])
 
-    date = datetime.datetime.strptime(user_date, '%d-%b-%Y')
-
-    #create_date_dict(rain_data)
+    # year = input(f"Please enter a year {list(rain_data.keys())[-1]}-{list(rain_data.keys())[0]}: ")
     
+    # months = list(rain_data[year].keys())[::-1]
+    # month = input(f"Please enter a month 1-12: ")
+    # month = months[int(month)-1]
+
+    # print(month)
+
+    # days = list(rain_data[year][month].keys())[::-1]
+
+    # print(days)
+
+    # day = input(f"Enter a day {days[0]}-{days[-1]}: ")
+    # day = days[int(day.strip("0"))]
+
+    # print(day)
+    # print(f"The total rainfall for {month}-{year} was {rain_data[year][month]}")
 main()
 
