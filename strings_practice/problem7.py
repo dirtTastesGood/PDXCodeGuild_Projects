9ec6c37095613276da27f2308598b7c339a1b281
unknown <keegood8@gmail.com>
2019-10-17 21:22:29 -0700

    problem7. Convert input strings to lowercase without any surrounding whitespace.

---
+++ strings_practice/problem7.py
@@ -0,0 +1,7 @@
+def lower_case(user_string):
+    return user_string.strip(" ").lower()
+
+def main():
+    print(lower_case("        NANNANANANA BATMAN        "))
+    print(lower_case("SUPER!"))
+main()
