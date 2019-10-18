e86e1eadb35799892a8319d79171eb3b91453ca0
unknown <keegood8@gmail.com>
2019-10-17 19:37:53 -0700

    Add variance calculation. Change average function.

--- rain_data/rain_data.py
+++ rain_data/rain_data.py
@@ -4,7 +4,7 @@ def load_data(data):
         lines = []
         for line in data:
             line = line.split(" ")
-            line = [x for x in line if x != ""] # eliminate blank strings in line
+            line = [x for x in line if x != ""] # keep only the elements that are not blank strings
             lines.append([line[0], line[1]])
 
     return lines
@@ -13,11 +13,11 @@ def create_date_dict(data):
     date_dict = {}
 
     for datum in data:
-        split_date = datum[0].split("-")
+        date = split_date(datum[0])
         
-        year = split_date[2]
-        month = split_date[1]
-        day = split_date[0]
+        year = date[2]
+        month = date[1]
+        day = date[0]
 
         rainfall = datum[1]
 
@@ -29,41 +29,67 @@ def create_date_dict(data):
                 date_dict[year][month] = {day:rainfall} 
             elif month in date_dict[year]:
                 date_dict[year][month][day] = rainfall
-            
+    
     return date_dict
 
-def average(data):
-    nums = []
-    for i in data:
-        if i[1] != '-':
-            nums.append(int(i[1]))
-    avg = sum(nums) // len(nums)
-
-    return avg
+def split_date(date):
+    return date.split("-")
     
+def get_average(data):
+    nums = []
+    count = 0
+    for i in data:
+        for j in data[i]:
+            for k in data[i][j]:
+                num = data[i][j][k]
+                if num != "-":
+                    nums.append(int(num))
+                    count += 1
+    
+    avg = sum(nums) / count
+    
+    return avg
+
+def get_variance(data, avg):
+    nums = []
+    count = 0
+    for i in data:
+        for j in data[i]:
+            for k in data[i][j]:
+                num = data[i][j][k]
+                if num != "-":
+                    num = int(num)
+                    nums.append((num - avg) ** 2)
+                    count += 1
+    
+    variance = sum(nums) / count
+    
+    return variance
+
 def main():
     raw_data = "rain_data.txt"
     data = load_data(raw_data)
     rain_data = create_date_dict(data)
 
-    year = input(f"Please enter a year {list(rain_data.keys())[-1]}-{list(rain_data.keys())[0]}: ")
+    # year = input(f"Please enter a year {list(rain_data.keys())[-1]}-{list(rain_data.keys())[0]}: ")
     
-    months = list(rain_data[year].keys())[::-1]
-    month = input(f"Please enter a month 1-12: ")
-    month = months[int(month)-1]
+    # months = list(rain_data[year].keys())[::-1]
+    # month = input(f"Please enter a month 1-12: ")
+    # month = months[int(month)-1]
 
-    days = list(rain_data[year][month].keys())[::-1]
+    # days = list(rain_data[year][month].keys())[::-1]
 
-    day = input(f"Enter a day {days[0]}-{days[-1]}: ")
-    if day[0] == "0":
-        day = day.strip("0")
+    # day = input(f"Enter a day {days[0]}-{days[-1]}: ")
+    # if day[0] == "0":
+    #     day = day.strip("0")
     
-    day = days[int(day)-1]
+    # day = days[int(day)-1]
 
-    print(day)
-    print(f"The total rainfall for {month}-{day}-{year} was {rain_data[year][month][day]}")
+    # print(f"The total rainfall for {month}-{day}-{year} was {rain_data[year][month][day]}")
 
-    print(f"The average total rainfall over the entire data set is: {average(data)}")
+    avg = get_average(rain_data)
+    print(f"The average total rainfall over the entire data set is: {avg}")
+
+    print(f"The variance over the entire data set is: {get_variance(rain_data, avg)}")
     
 main()
-
