18eb5db7b70ed5e6a868ddd884a6becc9d4ed4af
unknown <keegood8@gmail.com>
2019-10-19 22:27:35 -0700

    ATM. define Account and ATM classes and methods for each

---
+++ ATM/ATM.py
@@ -0,0 +1,48 @@
+class Account:
+    def __init__(self, accnt_num, name, rate):
+        self.accnt_num = accnt_num
+        self.owner = name
+        self.balance = 0
+        self.interest_rate = rate
+
+        return
+
+class ATM:
+    def __init__(self):
+        self.accounts = {}
+        self.interest_rate = 0.1
+        return 
+
+    def create_account(self):
+        
+        return
+
+    def __gen_accnt_num(self):
+       
+        return 
+
+    def check_balance(self, account):
+        return 
+
+    def deposit(self, account, amount):
+        return
+
+    def __check_withdrawal(self, account, amount):
+
+        return
+
+    def withdraw(self, account, amount):
+        return
+
+    def calc_interest(self, account):
+        return
+
+    def __save_accounts(self):
+        
+        pass
+
+def main():
+    return
+    
+    
+main()
