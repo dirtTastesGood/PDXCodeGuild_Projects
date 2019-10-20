9f3276056cb025a3e0290845a8e2791327bf93cf
unknown <keegood8@gmail.com>
2019-10-19 22:33:26 -0700

    generate an account number, create an account by adding Account() to atm.accounts

--- ATM/ATM.py
+++ ATM/ATM.py
@@ -14,12 +14,38 @@ class ATM:
         return 
 
     def create_account(self):
-        
+        name = input("\nPlease enter your name: ")
+    
+        accnt_num = self.__gen_accnt_num()
+
+        self.accounts[accnt_num] = Account(accnt_num, name, self.interest_rate)
+
         return
 
     def __gen_accnt_num(self):
-       
-        return 
+        if len(self.accounts) == 0:
+            accnt_num = "0001"
+
+        else:
+            keys = list(self.accounts.keys())
+            num = self.accounts[keys[-1]]['accnt_num'] # grab the last account number in dictionary
+
+            zeros_needed = 0
+            i=0
+            while True:
+                if num[i] != '0':
+                    num = num[i::]
+                    break
+                i += 1    
+                zeros_needed = i
+                
+            num = int(num) + 1
+
+            zeros = ''.join(["0" for i in range(0, zeros_needed) if len(str(num)) < 4])
+
+            accnt_num = f"{zeros}{num}"
+
+        return accnt_num
 
     def check_balance(self, account):
         return 
@@ -42,7 +68,9 @@ class ATM:
         pass
 
 def main():
+    atm = ATM()
+    atm.create_account()
+    print(atm.accounts['0001'])
     return
-    
     
 main()
