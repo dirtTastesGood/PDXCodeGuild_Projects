70f15f3716b4c3d0f404cbb214f1c65e7487dd22
unknown <keegood8@gmail.com>
2019-11-18 21:14:49 -0800

    added pick6() and countMatches()

--- Javascript/lab1/pick6/pick6.js
+++ Javascript/lab1/pick6/pick6.js
@@ -1,5 +1,29 @@
+function pick6(){
+    let ticket = [];
+
+    for(let i=0; i<6; i++){
+        ticket.push(Math.ceil(Math.random()*99));
+    }
+
+    return ticket
+}
+
+function countMatches(ticket, winningTicket){
+    let match_count = 0;
+
+    return match_count
+}
+
 function main(){
     const readline = require('readline-sync');
+
+    const prizes = [0, 4, 7, 100, 50000, 1000000, 25000000];
+    const winningTicket = pick6();
+    const ticket_cost = 2;
+    let expenses = 0;
+    
+    let playerTicket = pick6();
+    
 };
 
 main();
