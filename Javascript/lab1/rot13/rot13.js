8913e99fa6df3e5e1489450321b98ff31357256d
unknown <keegood8@gmail.com>
2019-11-19 19:02:46 -0800

    Versions 1 and 2.

--- Javascript/lab1/rot13/rot13.js
+++ Javascript/lab1/rot13/rot13.js
@@ -1,4 +1,33 @@
+function rotateString(str, offset){
+    let abc = "abcdefghijklmnopqrstuvwxyz";
+    
+    offset = offset % abc.length;
+
+    let abcLeft = abc.slice(0, offset);
+    let abcRight = abc.slice(offset, abc.length);
+    
+    let abcRotated = abcRight + abcLeft;
+    
+    strArr = str.split('');
+
+    for(let i=0; i<strArr.length; i++){
+        if(strArr[i].match(/([a-zA-Z])/)){
+            let index = abc.indexOf(strArr[i]);
+            let newChar = abcRotated[index];
+            
+            strArr[i] = newChar;
+        }
+    }
+    return strArr.join('');
+};
+
 function main(){
+    const readline = require('readline-sync');
+
+    let userString = readline.question("Please enter a string: ");
+    let rotation = parseInt(readline.question("Please enter the rotation: "));
+
+    console.log(`Rotated: ${rotateString(userString, rotation)}`);
 
 };
 
