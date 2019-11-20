6906c6f531b76c306915ff609144bbad8e0a294b
unknown <keegood8@gmail.com>
2019-11-19 19:41:39 -0800

    add support for capital letters

--- Javascript/lab1/rot13/rot13.js
+++ Javascript/lab1/rot13/rot13.js
@@ -1,20 +1,29 @@
 function rotateString(str, offset){
     let abc = "abcdefghijklmnopqrstuvwxyz";
-    
+    let ABC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
     offset = offset % abc.length;
 
     let abcLeft = abc.slice(0, offset);
     let abcRight = abc.slice(offset, abc.length);
     
+    let ABCLeft = ABC.slice(0, offset);
+    let ABCRight = ABC.slice(offset, ABC.length);
+
     let abcRotated = abcRight + abcLeft;
+    let ABCRotated = ABCRight + ABCLeft;
     
     strArr = str.split('');
 
     for(let i=0; i<strArr.length; i++){
-        if(strArr[i].match(/([a-zA-Z])/)){
-            let index = abc.indexOf(strArr[i]);
+        if(strArr[i].match(/([A-Z])/)){
+            let index = ABC.indexOf(strArr[i]);
+            let newChar = ABCRotated[index];
+            strArr[i] = newChar;
+
+        } else if(strArr[i].match(/([a-z])/)){            
+            let index = abc.indexOf(strArr[i]);            
             let newChar = abcRotated[index];
-            
             strArr[i] = newChar;
         }
     }
