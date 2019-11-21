1446f6ea5068a326f6b1e2c26dd43c3bd4cc3c60
unknown <keegood8@gmail.com>
2019-11-20 19:03:26 -0800

    Recursively determine if a string is a palindrome.

--- Javascript/extras/palindrome.js
+++ Javascript/extras/palindrome.js
@@ -1,4 +1,32 @@
+
+function isPalindrome(aString, index=0){
+    aString = aString.replace(/\s/g, '').toLowerCase();
+
+    while(true){
+        let first = index;
+        let last = aString.length-(index+1);
+        if(aString[first] === aString[last]){
+            aString = aString.slice(first+1, last);
+            index++;
+            
+            if(aString.length <= 1){
+                return true
+            }
+
+            isPalindrome(aString, index);
+        } else {
+            return false
+        }
+    }
+}
+
 function main(){
+    console.log(isPalindrome("t a  co cat"));
+    console.log(isPalindrome("t a  co cadt"));
+    console.log(isPalindrome("A nut for a jar of tuna"));
+    
+
+
 
 }
 
