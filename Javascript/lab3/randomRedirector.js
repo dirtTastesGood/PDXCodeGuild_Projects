ea7c8dcb069b8e3061cabba02ec7b0c81e7a6b68
unknown <keegood8@gmail.com>
2019-11-25 20:03:52 -0800

    lab 3. version 2, redirect after 5-second countdown

--- Javascript/lab3/randomRedirector.js
+++ Javascript/lab3/randomRedirector.js
@@ -1,5 +1,13 @@
 window.addEventListener('load', () => {
-    const urls = ['https://pbfcomics.com/', 'https://sarahcandersen.com/', 'https://www.boltcityproductions.com/', 'http://www.rice-boy.com/', 'https://abstrusegoose.com/'];
-    let choice = Math.floor(Math.random() * urls.length);
-    window.location = urls[choice];
+    let counter = 5;
+    const timer = setInterval(() => {
+                document.querySelector('.count').innerText = counter;
+                counter--;
+
+                if(counter < 0){
+                    const urls = ['https://pbfcomics.com/', 'https://sarahcandersen.com/', 'https://www.boltcityproductions.com/', 'http://www.rice-boy.com/', 'https://abstrusegoose.com/'];
+                    let choice = Math.floor(Math.random() * urls.length);
+                    window.location = urls[choice];
+                }
+        }, 1000);
 });
