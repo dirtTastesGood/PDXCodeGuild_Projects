b233e3a255cb5039f17c77dfee5700d81de09cdd
unknown <keegood8@gmail.com>
2019-11-27 20:52:45 -0800

    dynamically create a quote card for each quote in the API response.

--- Javascript/lab9/randomQuote.js
+++ Javascript/lab9/randomQuote.js
@@ -1,5 +1,10 @@
-const callApi = () => {
-    axios.get('https://favqs.com/api/quotes/?page=2&filter=love', {
+const callApi = (page=1, filter) => {
+
+    filter = filter.replace("\"", "");
+
+    let url = `https://favqs.com/api/quotes/?page=${page}&filter=${filter}`;
+
+    axios.get(url, {
         headers: {
             'Authorization': `Token token="${favqs_api_key}"`
         }
@@ -10,18 +15,47 @@ const callApi = () => {
     });
 }
 
-// const sortQuotes = (quotes, filter) => {
+const buildQuoteCard = index => {
+    const quotesList = document.querySelector('#quotes-list');
 
-// }
+    let newQuote = document.createElement('div');
+    newQuote.classList.add('quote-card');
+    newQuote.id = `quote-card-${index}`;
+    
+    let lilQBody = document.createElement('div');
+    lilQBody.classList.add('quote-body');
+    lilQBody.id = `quote-body-${index}`;
+    newQuote.append(lilQBody);
+
+    let lilQAuthor = document.createElement('div');
+    lilQAuthor.classList.add('quote-author');
+    lilQAuthor.id = `quote-author-${index}`
+    newQuote.append(lilQAuthor);
+
+    quotesList.append(newQuote);
+}
 
 const displayQuotes = response => {
-    const qBody = document.querySelector('#quote-text');
-    const qAuthor = document.querySelector('#quote-author');
+    const quotesList = document.querySelector('#quotes-list');
+
+    const bigQBody = document.querySelector('#quote-text');
+    const bigQAuthor = document.querySelector('#quote-author');
 
     let quote = response.quotes[ Math.floor(Math.random() * response.quotes.length)]
 
-    qBody.innerText = quote.body;
-    qAuthor.innerText = quote.author;
+    bigQBody.innerText = quote.body;
+    bigQAuthor.innerText = quote.author;
+
+    for(let i=0; i<response.quotes.length; i++){
+        buildQuoteCard(i);
+
+        let qBody = document.querySelector(`#quote-body-${i}`);
+        let qAuthor = document.querySelector(`#quote-author-${i}`);
+
+        qBody.innerText = response.quotes[i].body;
+        qAuthor.innerText = response.quotes[i].author;
+    }
+
 }
 
-callApi();
+callApi(2,"inspiration");
