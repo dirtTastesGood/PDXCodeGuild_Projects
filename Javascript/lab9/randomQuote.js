5cf3b8cdc6e089df29f43a34d14b53fed64d3485
unknown <keegood8@gmail.com>
2019-11-27 19:50:17 -0800

    add quotes-list section with search bar and filter options, author list and quotes list

--- Javascript/lab9/randomQuote.js
+++ Javascript/lab9/randomQuote.js
@@ -1,19 +1,22 @@
 const callApi = () => {
-    axios.get('https://favqs.com/api/quotes', {
+    axios.get('https://favqs.com/api/quotes/?page=2&filter=love', {
         headers: {
             'Authorization': `Token token="${favqs_api_key}"`
         }
     })
     .then(response => {
-        displayQuote(response.data);
+        displayQuotes(response.data);
         console.log(response.data);
-        
     });
 }
 
-const displayQuote = response => {
+// const sortQuotes = (quotes, filter) => {
+
+// }
+
+const displayQuotes = response => {
     const qBody = document.querySelector('#quote-text');
-    const qAuthor = document.querySelector('#quote-author');    
+    const qAuthor = document.querySelector('#quote-author');
 
     let quote = response.quotes[ Math.floor(Math.random() * response.quotes.length)]
 
