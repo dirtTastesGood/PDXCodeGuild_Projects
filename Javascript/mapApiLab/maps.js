4a1669cad8133b87f68cb600052678d0144745e2
unknown <keegood8@gmail.com>
2019-12-02 19:08:47 -0800

    add map data with Leaflet and Mapbox API

---
+++ Javascript/mapApiLab/maps.js
@@ -0,0 +1,10 @@
+let myMap = L.map('map').setView([52.505, -0.09], 13);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    accessToken: `${mapbox_token}`
+}).addTo(myMap);
+
+let marker = L.marker([51.5, -0.09]).addTo(myMap);
