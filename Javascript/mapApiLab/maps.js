93a704117397185c1fcf15f7632412b0931621e9
unknown <keegood8@gmail.com>
2019-12-03 19:02:54 -0800

    abandon circle style on map. generate markers on square map

--- Javascript/mapApiLab/maps.js
+++ Javascript/mapApiLab/maps.js
@@ -1,10 +1,39 @@
-let myMap = L.map('map').setView([52.505, -0.09], 13);
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    accessToken: `${mapbox_token}`
-}).addTo(myMap);
+const generateMap = (lat, lng, zoom) => {
+    let myMap = L.map('map').setView([lat, lng], zoom);
 
-let marker = L.marker([51.5, -0.09]).addTo(myMap);
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a><br/> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        accessToken: `${mapbox_token}`
+    }).addTo(myMap);
+
+    return myMap
+}
+
+const generateMarkers = (map, markerCount) => {
+
+    let left = map.getBounds()._southWest.lng;
+    let right = map.getBounds()._northEast.lng;
+    let top = map.getBounds()._northEast.lat;
+    let bottom = map.getBounds()._southWest.lat;
+
+    for(let i=0; i<markerCount; i++){
+        let lat = Math.random() * (bottom - top) + top;
+        let lng = Math.random() * (right - left) + left;
+
+        L.marker([lat,lng]).addTo(map);
+    }
+}
+
+const main = () => {
+    let lat = 45.523064;
+    let lng = -122.676483;
+    let zoom = 3;
+    
+    const myMap = generateMap(lat,lng,zoom);
+    generateMarkers(myMap, 10);
+}
+
+main();
