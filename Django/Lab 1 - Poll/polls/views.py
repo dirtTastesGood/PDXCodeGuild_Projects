819c9f7a26ac6fc98eed9eeb1cb9148860e0a721
unknown <keegood8@gmail.com>
2019-12-13 20:11:44 -0800

    fix vote view to redirect to results. style results page

--- Django/Lab 1 - Poll/polls/views.py
+++ Django/Lab 1 - Poll/polls/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
+from django.urls import generic
 
 from .models import Choice, Question
 
-# Create your views here.
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     template = loader.get_template('polls/index.html')
@@ -27,11 +27,8 @@ def results(request, question_id):
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
-        print('')
-        print(request.POST.items)
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
-        print('except')
         return render(request, 'polls/detail.html', {
             'question':question,
             'error_message':"You didn't select a choice."
@@ -40,8 +37,4 @@ def vote(request, question_id):
         selected_choice.votes += 1
         selected_choice.save()
         
-        print("")
-        print('called')
-        print("")
-
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
