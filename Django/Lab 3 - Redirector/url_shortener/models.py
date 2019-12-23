e4ed79b182384bf9968fc11c20b9b0d27b245656
unknown <keegood8@gmail.com>
2019-12-23 12:45:26 -0800

    create CustomURL model

--- Django/Lab 3 - Redirector/url_shortener/models.py
+++ Django/Lab 3 - Redirector/url_shortener/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class CustomURL(models.Model):
+    url = models.CharField(max_length=300)
+    code = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'URL code: {self.code}'
