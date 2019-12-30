efa1e54d1136dcf27639e28ed25ec88efcd94387
unknown <keegood8@gmail.com>
2019-12-29 23:26:45 -0800

    change urls and views in order to pass long_url from input form to saveurl() view

--- Django/Lab 3 - Redirector/url_shortener/urls.py
+++ Django/Lab 3 - Redirector/url_shortener/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='url_shortener_home'),
+    path('saveurl/', views.saveurl, name='url_shortener_saveurl'),
+    path('redirect/', views.redirect_it, name='url_shortener_redirect_it')
 ]
--- Django/Lab 3 - Redirector/url_shortener/views.py
+++ Django/Lab 3 - Redirector/url_shortener/views.py
@@ -1,4 +1,36 @@
-from django.shortcuts import render
+import string, random
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+
+from .models import CustomURL
 
 def index(request):
     return render(request, 'url_shortener/index.html')
+
+def saveurl(request):
+    if(request.method == 'POST'):
+
+        print('')
+        print('')
+        print('')
+
+        print('CALLED!')
+        print(f"long_url: {request.POST['long_url']}")
+
+    print('')
+    print('')
+    print('')
+
+    new_code = ''
+
+    for i in range(6):
+        new_code += random.choice(string.ascii_lowercase + string.digits)
+
+    try:
+        new_url = CustomURL.objects.get(code=new_code)
+    except CustomURL.DoesNotExist:
+        new_url = CustomURL.objects.create(url=request.POST['long_url'], code=new_code)
+    return redirect('/')
+
+def redirect_it(request):
+    pass
